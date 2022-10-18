195f791a649f650bd71e254e68e499676a4d9bc9
tolulope55 <olonts@gmail.com>
2022-10-18 01:20:03 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import DesignAss from './component/DesignAss';
+
+function App() {
+  return (
+    <div className="App">
+    <DesignAss />
+    </div>
+  );
+}
+
+export default App;
